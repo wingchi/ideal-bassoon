742af5253063a79f327bfde5057db239d8d0a7bb
Stacey <staceyschlanger@gmail.com>
2016-02-18 20:12:16 -0500

    initial javascript commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//grab the data
+
+var parseHeaders = {
+  "X-Parse-Application-Id": "SeLYMXyUE2tACKJqP26AT7SwyOxoiXNMXmZlu7z4",
+  "X-Parse-REST-API-Key": "9O3k5xscPzXuTZp6PLWLxZtJwSVgryfChcb72UbK",
+  "Content-Type": "application/json",
+};
+
+
+//This code gets from Parse
+$.ajax({
+  url: "https://api.parse.com/1/classes/Chapter",
+  method: "GET",
+  headers: parseHeaders
+}).done( function(data){
+  $('#wrapper').text(JSON.stringify(data));
+});
